26aee315814fb91a5b0b36eee0d2e6ee9f6c8fe8
mojahed mahdi <mom77909@gmail.com>
2025-09-26 17:49:42 +0000

    https://github.com/6thd/hasebny-payroll-system اريدك ان ترفع ملفات المشر

---
+++ push_to_github.sh
@@ -0,0 +1,28 @@
+
+#!/bin/bash
+echo "===================================================================="
+echo "== This script will upload your project to GitHub.                =="
+echo "== Make sure you have Git installed on your system.               =="
+echo "===================================================================="
+
+# Initialize a Git repository if it doesn't exist
+git init -b main
+
+# Add all the files to be tracked
+git add .
+
+# Commit the files with a message
+git commit -m "Initial commit of Hasebny Payroll System"
+
+# Add your GitHub repository as the remote destination
+git remote add origin https://github.com/6thd/hasebny-payroll-system.git
+
+# Push the code to GitHub
+echo "Pushing files to GitHub..."
+git push -u origin main
+
+echo ""
+echo "===================================================================="
+echo "== Done! Your files have been uploaded to your GitHub repository. =="
+echo "===================================================================="
+read -p "Press any key to continue..."
